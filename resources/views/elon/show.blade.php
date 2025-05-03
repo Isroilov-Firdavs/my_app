58e0cd7470a08df945d3079e8a4599d840df6e3d
Isroilov Firdavs <firdavsisroilov28@gmail.com>
2025-05-04 00:58:59 +0500

    Posts show.blade

--- resources/views/elon/show.blade.php
+++ resources/views/elon/show.blade.php
@@ -13,17 +13,19 @@
             </div>
             <div class="col-lg-4 border p-3">
                 <div class="border">
-                    <h3 class="p-1">{{ $post->title }}</h3>
-                    <p class="p-1">{{ $post->body }}</p>
-                    <p class="p-1">{{ $post->updated_at }}</p>
-                    <p class="p-1">{{ \Carbon\Carbon::parse($post->created_at)->format('d.m.Y') }}</p>
-                    <a class="btn btn-warning btn-sm w-100 mt-1" href="{{ route('posts.edit', $post) }}">Tahrirlash</a>
-                    <form action="{{ route('posts.destroy', $post) }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm w-100 mt-1" onclick="return confirm('Ishonchingiz komilmi?')">O'chirish</button>
-                    </form>
-                    <a class="btn btn-success btn-sm w-100 mt-1" href="{{ route('posts.index') }}">Orqaga</a>
+                    <div class="m-1">
+                        <h3 class="p-1">{{ $post->title }}</h3>
+                        <p class="p-1">{{ $post->body }}</p>
+                        <p class="p-1">{{ $post->updated_at }}</p>
+                        <p class="p-1">{{ \Carbon\Carbon::parse($post->created_at)->format('d.m.Y') }}</p>
+                        <a class="btn btn-warning btn-sm w-100 mt-1" href="{{ route('posts.edit', $post) }}">Tahrirlash</a>
+                        <form action="{{ route('posts.destroy', $post) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm w-100 mt-1" onclick="return confirm('Ishonchingiz komilmi?')">O'chirish</button>
+                        </form>
+                        <a class="btn btn-success btn-sm w-100 mt-1" href="{{ route('posts.index') }}">Orqaga</a>
+                    </div>
                 </div>
             </div>
         </div>
